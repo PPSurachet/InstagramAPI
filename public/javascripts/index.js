a3afaca1938fe5557fe1fa790cc0a6a6868d5604
DOUBLEPOR <doublepor.pp$gmail.com>
2020-09-22 19:46:51 +0700

    Add : Search By Shortcode

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,5 @@
 $(async function () {
     const APIurl = "https://insta-api-por.herokuapp.com/api/posts";
-
-
     try {
         const response = await axios.get(APIurl);
         console.log(response);
@@ -19,16 +17,47 @@ $(async function () {
         console.log(error);
     }
 
-    // $.get(APIurl, function (data, status) {
-    //     console.log(data);
-    //     $("#posts").empty();
-    //     for (const postid in data.data) {
-    //         const post = data.data[postid];
-    //         let row = `<tr>
-    //                     <th scope="row">${post.shortcode}</th>
-    //                     <td>${post.full_name}</td>
-    //                 </tr>`;
-    //         $("#posts").append(row);
-    //     }
-    // });
+    try {
+        const response = await axios.get(APIurl);
+        $("#SearchID").click(function () {
+            // $("#SearchPosts").empty();
+            const ID = $("#ID").val();
+            const post = response.data.data[ID - 1];
+            let row = `<tr>
+                        <th scope="row">${post.id}</th>
+                        <td>${post.shortcode}</td>
+                        <td>${post.full_name}</td>
+                        <td>${post.location_name}</td>
+                    </tr>`;
+            $("#SearchPosts").append(row);
+        })
+
+        $("#SearchSC").click(function () {
+            // $("#SearchPosts").empty();
+            const Shortcode = $("#SC").val();
+            for (const postid in response.data.data) {
+                const post = response.data.data[postid];
+                if (post.shortcode == Shortcode) {
+                    let row = `<tr>
+                        <th scope="row">${post.id}</th>
+                        <td>${post.shortcode}</td>
+                        <td>${post.full_name}</td>
+                        <td>${post.location_name}</td>
+                    </tr>`;
+                    $("#SearchPosts").append(row);
+                }
+            }
+            // let row = `<tr>
+            //             <th scope="row">${post.id}</th>
+            //             <td>${post.shortcode}</td>
+            //             <td>${post.full_name}</td>
+            //             <td>${post.location_name}</td>
+            //         </tr>`;
+            // $("#SearchPosts").append(row);
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+
 })
